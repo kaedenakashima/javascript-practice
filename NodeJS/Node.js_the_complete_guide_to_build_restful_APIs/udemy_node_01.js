ca800fd96cb4f691b81d1b0ee62c05e3c910110a
Kaede Nakashima <kaede.nakashima@yahoo.com>
2019-12-11 12:07:36 +0100

    Add files via upload

---
+++ NodeJS/Node.js_the_complete_guide_to_build_restful_APIs/udemy_node_01.js
@@ -0,0 +1,14 @@
+
+available code in Node
+console.log();//global
+setTimeout()
+clearTimeout();
+
+setInterval()
+clearInterval()
+
+window.console.log
+ver message = '';
+global.setTimeout
+console.log(global.message);
+window.message
