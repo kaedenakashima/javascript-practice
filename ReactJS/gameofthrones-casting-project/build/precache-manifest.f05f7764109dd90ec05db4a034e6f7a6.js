7bd2da07f204f79884636b4e434f45de250dca70
Kaede Nakashima <kaede.nakashima@yahoo.com>
2020-05-09 09:00:35 +0900

    Add files via upload

---
+++ ReactJS/gameofthrones-casting-project/build/precache-manifest.f05f7764109dd90ec05db4a034e6f7a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa68f0bfa6df7865801ef5ae0a2579e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0314d1816ccb99af0a2a",
+    "url": "/static/css/main.48b70ab0.chunk.css"
+  },
+  {
+    "revision": "c5b9a65354e6475ccb12",
+    "url": "/static/js/2.b502a0eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b502a0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0314d1816ccb99af0a2a",
+    "url": "/static/js/main.ba148e71.chunk.js"
+  },
+  {
+    "revision": "733c63775b221c047f89",
+    "url": "/static/js/runtime-main.b829c87b.js"
+  },
+  {
+    "revision": "76e49fafd01d4dca587da88e49937dee",
+    "url": "/static/media/emilia_clarke.76e49faf.jpg"
+  },
+  {
+    "revision": "184960d6627580b6c7dab7e3735060fe",
+    "url": "/static/media/gameofthrones_logo.184960d6.jpeg"
+  },
+  {
+    "revision": "84f22b1f1a92a169018387db8cf72858",
+    "url": "/static/media/maisie_williams.84f22b1f.jpg"
+  }
+]);
