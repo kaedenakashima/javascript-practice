1d5003ba3d8c90a551a8d24c9b5fe092594aeccd
Kaede Nakashima <kaede.nakashima@yahoo.com>
2020-05-21 08:26:25 +0900

    Add files via upload

---
+++ ReactJS/the-post-app2(undone)/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Blog from './containers/Blog/Blog'
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat :200, 400, 600, 800', 'Raleway :100, 400, 600, 800', 'Amiri: 400'],
+  },
+});
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App" >
+          <Blog />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
